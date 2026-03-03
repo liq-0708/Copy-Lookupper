1c868c83fba7494f75de207a133be97c108ffc15
liq-0708 <guanlum1@gmail.com>
2026-03-03 22:17:16 +0800

    Initial Commit

---
+++ Copy Lookupper/Copy_LookupperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Copy_LookupperApp.swift
+//  Copy Lookupper
+//
+//  Created by skylar on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Copy_LookupperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
